5c7f3198ee4e81315d9ea9e823b3e0959a3affb6
kennydevstudios <awodirekehinde11@gmail.com>
2026-06-05 15:12:19 +0100

    initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,103 @@
+const counters = document.querySelectorAll('.counter');
+
+const startCounter = () => {
+    counters.forEach(counter => {
+
+        const target = +counter.dataset.target;
+        let count = 0;
+        const update = () => {
+
+            count++;
+            counter.innerText = count;
+            if(count < target){
+                setTimeout(update,200);
+            }
+        }
+        update();
+    });
+};
+
+const text =
+"Full-Stack Web Developer & Ecommerce Specialist";
+
+const typingText =
+document.getElementById("typing-text");
+let index = 0;
+function type(){
+    if(index < text.length){
+        typingText.textContent += text.charAt(index);
+        index++;
+        setTimeout(type,60);
+    }
+}
+type();
+
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+    reveals.forEach(section => {
+
+        const sectionTop =
+        section.getBoundingClientRect().top;
+
+        if(sectionTop < window.innerHeight - 100){
+            section.classList.add('active');
+        }
+    });
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+            startCounter();
+            observer.disconnect();
+        }
+    });
+});
+observer.observe(document.querySelector('.hero-stats'));
+
+const sections =
+document.querySelectorAll('section');
+
+const navLinks =
+document.querySelectorAll('.nav-links a');
+
+window.addEventListener('scroll',()=>{
+    let current='';
+
+    sections.forEach(section=>{
+        const sectionTop =
+        section.offsetTop;
+
+        if(pageYOffset >= sectionTop - 200){
+            current = section.getAttribute('id');
+        }
+    });
+    navLinks.forEach(link=>{
+
+        link.classList.remove('active');
+        if(link.href.includes(current)){
+            link.classList.add('active');
+        }
+    });
+});
+const scrollBtn =
+document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > 300){
+
+        scrollBtn.style.display="block";
+    }else{
+
+        scrollBtn.style.display="none";
+    }
+});
+
+scrollBtn.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
